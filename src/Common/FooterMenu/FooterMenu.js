c4cacdf12e5572dd4ab92885f31f4e9f85807c08
michikogo <37466954+michikogo@users.noreply.github.com>
2021-03-16 06:48:55 +0800

    Created footer layout

--- src/Common/FooterMenu/FooterMenu.js
+++ src/Common/FooterMenu/FooterMenu.js
@@ -1,6 +1,26 @@
+import { Row, Col } from "react-bootstrap";
+import { logo } from "../../Assets/index";
 const Footer = () => {
   return (
-    <div style={{ textAlign: "center" }}>BAKERYMNL Created by Aoki Young</div>
+    <div
+      style={{
+        backgroundColor: "#4A6C6F",
+        textAlign: "center",
+        padding: "16px 8px",
+      }}
+    >
+      <img src={logo} />
+      <div
+        style={{
+          paddingTop: "15px",
+          fontFamily: "Patrick Hand",
+          fontSize: "20px",
+          color: "white",
+        }}
+      >
+        Created by Aoki Young
+      </div>
+    </div>
   );
 };
 
