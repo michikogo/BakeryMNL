fbb828279cbf7ce602d93911fa5e3757cece1e97
michikogo <37466954+michikogo@users.noreply.github.com>
2021-03-16 10:10:21 +0800

    created specific product

--- src/Pages/Product/Product.js
+++ src/Pages/Product/Product.js
@@ -1,10 +1,72 @@
-import React from "react";
-
+import { useState, useEffect } from "react";
+import { useParams } from "react-router";
+import { Container, Row, Col, Image, Card, Button } from "react-bootstrap";
+import "./index.css";
 const Product = () => {
+  const { id } = useParams();
+  const [item, setItem] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/products/" + id)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setItem(data);
+        console.log(item);
+      });
+  }, []);
+
   return (
-    <div>
-      <h1>Item Page</h1>
-    </div>
+    <Container style={{ padding: "0vh 30px" }}>
+      {item && (
+        <Card style={{ padding: "30px" }}>
+          <Card.Body>
+            <h5 style={{ textAlign: "center", paddingBottom: "30px" }}>
+              {item.title}
+            </h5>
+            <Row>
+              <Col sm={4} style={{ textAlign: "right" }}>
+                <Image
+                  src={require("../../Assets/Image" + item.imageURL).default}
+                  alt={item.imageURL}
+                  fluid
+                  width="300px"
+                />
+              </Col>
+              <Col sm={8} style={{ paddingLeft: "30px", alignSelf: "center" }}>
+                <Row>
+                  <Col>
+                    <h6>Description:</h6>
+                    <div>{item.desc}</div>
+                  </Col>
+                </Row>
+                <Row style={{ paddingTop: "20px" }}>
+                  <Col>
+                    <h6>Price: ₱{item.price}</h6>
+                  </Col>
+                  <Col>
+                    <h6>Quantity: </h6>
+                  </Col>
+                </Row>
+                <Row style={{ textAlign: "center", paddingTop: "20px" }}>
+                  <Col sm={6}>
+                    <Button variant="success" block>
+                      Add To Order
+                    </Button>
+                  </Col>
+                  <Col sm={6}>
+                    <Button variant="primary" block>
+                      Buy Now
+                    </Button>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      )}
+    </Container>
   );
 };
 
