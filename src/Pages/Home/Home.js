6177b82e61855379e11a7fdda765f6506917f4b1
michikogo <37466954+michikogo@users.noreply.github.com>
2021-03-17 16:28:18 +0800

    Added Task to be done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,6 +4,19 @@ const Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <h3>Tasks:</h3>
+      <ul>
+        <li>home and about page - make design</li>
+
+        <li>item page - add quantity in db</li>
+        <li>order page - place loading on cart</li>
+
+        <li>order page - add remove button for items</li>
+
+        <li>order page - can change quantity</li>
+
+        <li>order page - remove when bought</li>
+      </ul>
     </div>
   );
 };
