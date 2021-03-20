6dfc149ca9e85c8b8d7c73f18ad0d5cababb87e6
michikogo <37466954+michikogo@users.noreply.github.com>
2021-03-20 15:46:33 +0800

    added layout

--- src/Assets/index.js
+++ src/Assets/index.js
@@ -1,6 +1,11 @@
 // Header Menu
 import logo from "./Image/logo.png";
 
+// Home Page
+import promo1 from "./Image/promo1.jpg";
+import promo2 from "./Image/promo2.jpg";
+import promo3 from "./Image/promo3.jpg";
+
 // About Page
 import item22 from "./Image/item22.jpg";
 import item23 from "./Image/item23.jpg";
@@ -20,6 +25,10 @@ import baking from "./Image/baking.jpg";
 export {
   // Header Menu
   logo,
+  // Home
+  promo1,
+  promo2,
+  promo3,
   // About Page
   item22,
   item23,
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,47 +1,78 @@
-import { useEffect } from "react";
+import "./index.css";
+import { useState, useEffect } from "react";
+import { Container, Carousel, CardDeck, Card } from "react-bootstrap";
+import { promo1, promo2, promo3 } from "../../Assets";
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [items, setItems] = useState(null);
+
   useEffect(() => {
     // Scroll to top
     window.scrollTo(0, 0);
     // Tab title
     document.title = "BakeryMNL | Home";
-  });
-  return (
-    <div>
-      <h1>Home Page</h1>
-      <h3>Tasks:</h3>
-      <ul>
-        <li>home, Not Found page - make design</li>
-        <li>product page add 3 random items for recomendation</li>
-        <li>Create a Read Me</li>
-        <li>research about deployment</li>
-        <li style={{ textDecoration: "line-through" }}>
-          order page - phone number should be numbers only
-        </li>
-        <li style={{ textDecoration: "line-through" }}>Tab Image Change</li>
-        <li style={{ textDecoration: "line-through" }}>Tab Title Change</li>
-        <li style={{ textDecoration: "line-through" }}>
-          Redirect go to top of the page
-        </li>
-        <li style={{ textDecoration: "line-through" }}>
-          order page - add remove button for items
-        </li>
-        <li style={{ textDecoration: "line-through" }}>
-          product - button cant be clicked if the user hasnt place quantity
-        </li>
-        <li style={{ textDecoration: "line-through" }}>
-          item page - add quantity in db
-        </li>
-        <li style={{ textDecoration: "line-through" }}>
-          order page - place loading on cart and error when cant be fetched
-        </li>
 
-        <li style={{ textDecoration: "line-through" }}>
-          order page - remove when bought
-        </li>
-      </ul>
-    </div>
+    fetch("http://localhost:8000/products")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setItems(data);
+      });
+  }, []);
+
+  const imageData = [promo1, promo2, promo3];
+  return (
+    <Container>
+      <Carousel fade>
+        {imageData.map((data) => (
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={data}
+              alt="First slide"
+              style={{ height: "226px", weidth: "690px", objectFit: "cover" }}
+            />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+
+      <CardDeck className="home-deck" style={{ paddingTop: "30px" }}>
+        {items &&
+          items.slice(0, 3).map((product) => (
+            <Card key={product.id} className="home-card">
+              {/* IMAGE */}
+              <Link to={`/bakery/${product.id}`}>
+                <Card.Img
+                  variant="top"
+                  src={require("../../Assets/Image" + product.imageURL).default}
+                  alt="image"
+                  className="home-image"
+                />
+              </Link>
+              {/* TITLE */}
+              <Card.Body>
+                <Link className="home-title" to={`/bakery/${product.id}`}>
+                  <Card.Title>{product.title}</Card.Title>
+                </Link>
+              </Card.Body>
+              {/* PRICE */}
+              <Card.Footer>
+                <Link className="home-card-price" to={`/bakery/${product.id}`}>
+                  Price: ₱{product.price}
+                </Link>
+              </Card.Footer>
+              {/* LINK */}
+              <Card.Footer>
+                <Link className="home-card-order" to={`/bakery/${product.id}`}>
+                  Order Now!
+                </Link>
+              </Card.Footer>
+            </Card>
+          ))}
+      </CardDeck>
+    </Container>
   );
 };
 
