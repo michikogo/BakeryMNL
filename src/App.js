103ad76d28c4fe38ece5238e61574de1f5344c6f
michikogo <37466954+michikogo@users.noreply.github.com>
2021-03-21 14:24:18 +0800

    hash router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import HeaderMenu from "./Common/HeaderMenu/HeaderMenu";
 import FooterMenu from "./Common/FooterMenu/FooterMenu";
